d6bea6b67b5c9e34b5e3f85eee2f8b0e79348cce
vishal gehlot <vishalgehlot334@gmail.com>
2024-01-29 13:03:09 +0530

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const connectDb = require("./DbConnect");
+const cors = require("cors");
 const app = express();
 connectDb();
 
 app.use(express.json());
+app.use(cors());
 const userRouter = require("./src/routes/userRoute");
 app.use("/dev/api", userRouter);
 
